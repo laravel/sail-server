bd745bee8aeabe3bd5618e28a4d097532c3a06a3
Taylor Otwell <taylorotwell@gmail.com>
2020-10-29 15:51:07 -0500

    first

---
+++ resources/scripts/php74.sh
@@ -0,0 +1,34 @@
+curl http://localhost:8000/docker/7.4 | docker build -t sail/installer -
+
+docker run --rm \
+    -v $(pwd):/opt \
+    -w /opt \
+    sail/installer:latest \
+    composer create-project laravel/laravel {{ name }}
+
+cd {{ name }}
+
+CYAN='\033[0;36m'
+LIGHT_CYAN='\033[1;36m'
+NC='\033[0m'
+
+echo -e "${LIGHT_CYAN}"
+echo "             /|~~~"
+echo "           ///|"
+echo "         /////|"
+echo "       ///////|"
+echo "     /////////|"
+echo "   \==========|===/"
+echo "~~~~~~~~~~~~~~~~~~~~~"
+echo -e "${NC}"
+
+if sudo -n true 2>/dev/null; then
+    sudo chown -R $USER: .
+    echo -e "${CYAN}We hope you build something incredible. Get started with:${NC} cd {{ name }} && ./sail up"
+else
+    echo -e "${CYAN}Please provide your password so we can make some final adjustments to your application's permissions.${NC}"
+    echo ""
+    sudo chown -R $USER: .
+    echo ""
+    echo -e "${CYAN}Thank you! We hope you build something incredible. Dive in with:${NC} cd {{ name }} && ./sail up"
+fi
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/7.4/{name}', function ($name) {
+    return response(
+        str_replace('{{ name }}', $name, file_get_contents(resource_path('scripts/php74.sh'))),
+        200,
+        ['Content-Type' => 'text/plain']
+    );
+});
+
+Route::get('/docker/7.4', function () {
+    return response(
+        file_get_contents(resource_path('docker/php74.dockerfile')),
+        200,
+        ['Content-Type' => 'text/plain']
+    );
+});
