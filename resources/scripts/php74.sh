481137326d1383fe918bb18c77d178c7125347e8
Taylor Otwell <taylorotwell@gmail.com>
2020-11-11 10:23:08 -0600

    wip

--- resources/scripts/php74.sh
+++ resources/scripts/php74.sh
@@ -1,3 +1,12 @@
+docker info > /dev/null 2>&1
+
+# Ensure that Docker is running...
+if [ $? -ne 0 ]; then
+    echo "Docker is not running."
+
+    exit 1
+fi
+
 docker run --rm \
     -v $(pwd):/opt \
     -w /opt \
