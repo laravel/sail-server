b0246824c9a0813d961bc442bbf6df95b169b27a
AriaieBOY <ariaieboy.ir@gmail.com>
2024-12-30 19:29:09 +0330

    add valkey support (#28)
    * add valkey support
    
    * fix tests

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/{name}', function (Request $request, $name) {
         'pgsql',
         'mariadb',
         'redis',
+        'valkey',
         'memcached',
         'meilisearch',
         'typesense',
